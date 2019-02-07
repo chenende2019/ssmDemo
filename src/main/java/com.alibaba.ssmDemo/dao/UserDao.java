e4297946a21bf23cc61bb7ba0b77cf30def01152
chenende <m17600201360@163.com>
2019-02-08 02:55:49 +0800

    edit

--- src/main/java/com.alibaba.ssmDemo/dao/UserDao.java
+++ src/main/java/com.alibaba.ssmDemo/dao/UserDao.java
@@ -4,7 +4,6 @@ import com.alibaba.ssmDemo.model.User;
 import org.apache.ibatis.annotations.Param;
 
 public interface UserDao {
-    // 根据 id 寻找对应的 User
     User findUserById(int id);
     int insert(@Param("entry") User user);
 }
